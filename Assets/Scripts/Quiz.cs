d4353650ff0e1d459f7972de49a56507ea067156
Rock1r <2449brv@gmail.com>
2024-02-04 20:52:23 +0200

    Added button sprites changing and enabling/disabling buttons after answer

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,14 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System;
+using System.Reflection;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _questionText;
     [SerializeField] private QuestionSO _question;
     [SerializeField] private GameObject[] _answerButtons;
+    [SerializeField] private Sprite _defaultButtonSprite;
+    [SerializeField] private Sprite _correctButtonSprite;
+    int _correctAnswerIndex;
 
     void Start()
+    {
+        DisplayQuestion();
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        if (index == _question.GetCorrectAnswerIndex())
+        {
+            buttonImage = _answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = _correctButtonSprite;
+            _questionText.text = "Correct!";
+        }
+        else
+        {
+            _correctAnswerIndex = _question.GetCorrectAnswerIndex();
+            string correctAnswer = _question.GetAnswer(_correctAnswerIndex);
+            _questionText.text = "Sorry, the correct answer is:\n" + correctAnswer;
+            buttonImage = _answerButtons[_correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = _correctButtonSprite;
+        }
+        SetButtonsState(false);
+    }
+
+    private void GetNextQuestion()
+    {
+        SetButtonsState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+    }
+
+    private void DisplayQuestion()
     {
         _questionText.text = _question.GetQuestion();
 
@@ -17,6 +55,25 @@ public class Quiz : MonoBehaviour
         {
             TextMeshProUGUI buttonText = _answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = _question.GetAnswer(i);
+        }
+    }
+
+    private void SetButtonsState(bool state)
+    {
+        for (int i = 0; i < _answerButtons.Length; i++)
+        {
+            Button button = _answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
+   private void SetDefaultButtonSprites()
+    {
+        Image buttonImage;
+        for (int i = 0; i < _answerButtons.Length; i++)
+        {
+            buttonImage = _answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = _defaultButtonSprite;
         }
     }
 }
