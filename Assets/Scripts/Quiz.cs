aaf517779259bfa62300b1b2a4de92693f544540
Rock1r <2449brv@gmail.com>
2024-02-05 20:36:40 +0200

    Added timer and all questions

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -8,22 +8,79 @@ using System.Reflection;
 
 public class Quiz : MonoBehaviour
 {
+    [Header("Questions")]
     [SerializeField] private TextMeshProUGUI _questionText;
-    [SerializeField] private QuestionSO _question;
+    [SerializeField] private List<QuestionSO> _questions = new List<QuestionSO>();
+    private QuestionSO _currentQuestion;
+
+    [Header("Answers")]
     [SerializeField] private GameObject[] _answerButtons;
+    private int _correctAnswerIndex;
+    private bool _answerDone = false;
+
+    [Header("Button Colors")]
     [SerializeField] private Sprite _defaultButtonSprite;
     [SerializeField] private Sprite _correctButtonSprite;
-    int _correctAnswerIndex;
+
+    [Header("Timer")]
+    [SerializeField] private Image _timerImage;
+    private Timer _timer;
 
     void Start()
     {
-        DisplayQuestion();
+        _timer = FindObjectOfType<Timer>();
+    }
+
+    private void Update()
+    {
+        _timerImage.fillAmount = _timer._fillFraction;
+        if (_timer._loadNextQuestion)
+        {
+            GetNextQuestion();
+            _timer._loadNextQuestion = false;
+            _answerDone = false;
+        }
+        else if (!_answerDone && !_timer._isAnsweringQuestion)
+        {
+            DisplayAnswer(-1);
+            SetButtonsState(false);
+        }
     }
 
     public void OnAnswerSelected(int index)
     {
+        _answerDone = true;
+        DisplayAnswer(index);
+        SetButtonsState(false);
+        _timer.CancelTimer();
+    }
+
+    private void GetNextQuestion()
+    {
+        if (_questions.Count > 0)
+        {
+            SetButtonsState(true);
+            SetDefaultButtonSprites();
+            GetRandomQuestion();
+            DisplayQuestion();
+        }
+    }
+
+    private void GetRandomQuestion()
+    {
+        int index = UnityEngine.Random.Range(0, _questions.Count);
+        _currentQuestion = _questions[index];
+
+        if (_questions.Contains( _currentQuestion ) )
+        {
+            _questions.Remove(_currentQuestion);
+        }
+    }
+
+    private void DisplayAnswer(int index)
+    {
         Image buttonImage;
-        if (index == _question.GetCorrectAnswerIndex())
+        if (index == _currentQuestion.GetCorrectAnswerIndex())
         {
             buttonImage = _answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = _correctButtonSprite;
@@ -31,30 +88,22 @@ public class Quiz : MonoBehaviour
         }
         else
         {
-            _correctAnswerIndex = _question.GetCorrectAnswerIndex();
-            string correctAnswer = _question.GetAnswer(_correctAnswerIndex);
+            _correctAnswerIndex = _currentQuestion.GetCorrectAnswerIndex();
+            string correctAnswer = _currentQuestion.GetAnswer(_correctAnswerIndex);
             _questionText.text = "Sorry, the correct answer is:\n" + correctAnswer;
             buttonImage = _answerButtons[_correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = _correctButtonSprite;
         }
-        SetButtonsState(false);
-    }
-
-    private void GetNextQuestion()
-    {
-        SetButtonsState(true);
-        SetDefaultButtonSprites();
-        DisplayQuestion();
     }
 
     private void DisplayQuestion()
     {
-        _questionText.text = _question.GetQuestion();
+        _questionText.text = _currentQuestion.GetQuestion();
 
         for (int i = 0; i < _answerButtons.Length; i++)
         {
             TextMeshProUGUI buttonText = _answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = _question.GetAnswer(i);
+            buttonText.text = _currentQuestion.GetAnswer(i);
         }
     }
 
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float _timeToCompleteQuestion = 30f;
+    [SerializeField] private float _timeToShowCorrectAnswer = 5f;
+
+    private float _timerValue;
+
+    public float _fillFraction;
+    public bool _loadNextQuestion = false;
+    public bool _isAnsweringQuestion = false;
+
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    public void CancelTimer()
+    {
+        _timerValue = 0;
+    }
+
+    private void UpdateTimer()
+    {
+        _timerValue -= Time.deltaTime;
+
+        if(_isAnsweringQuestion)
+        {
+            if (_timerValue > 0)
+            {
+                _fillFraction = _timerValue / _timeToCompleteQuestion;
+            }
+            else
+            {
+                _timerValue = _timeToShowCorrectAnswer;
+                _isAnsweringQuestion = false;
+            }
+        }
+        else
+        {
+            if (_timerValue > 0)
+            {
+                _fillFraction = _timerValue / _timeToShowCorrectAnswer;
+            }
+            else
+            {
+                _timerValue = _timeToCompleteQuestion;
+                _isAnsweringQuestion = true;
+                _loadNextQuestion = true;
+            }
+        }
+
+        Debug.Log( _timerValue );
+    }
+}
